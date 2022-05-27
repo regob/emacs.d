affd1995b780e070a24b6530f49dad940c53de33
regob <rego97@gmail.com>
2022-05-27 16:32:47 +0200

    * Some package loading cleanup * Added projectile, multiple-cursors, smartparens, cc-mode configs * Setup for autopep8, removed autosave (due to infinite loops)

--- init.el
+++ init.el
@@ -1,7 +1,16 @@
-;; Set autosave directory
+;;; Emacs init file
+
+; use-package is not needed at runtime
+(eval-when-compile
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+; Set autosave directory
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
+
 (setq initial-buffer-choice "~")
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 120) (height . 80)))
 
 ; Remove toolbars, menu and scrollbars
 (menu-bar-mode -1)
@@ -12,10 +21,10 @@
 (add-hook 'after-init-hook (lambda() (load-theme 'tango-dark)))
 (set-face-attribute 'default nil
                     :family "SourceCodePro"
-                    :height 100
-                    )
+                    :height 100)
 
-;; other window to M-o (default C-x o)
+;
+; other window to M-o (default C-x o)
 (global-set-key (kbd "M-o") 'other-window)
 
 ;; windmove mode for S-<arrow> window navigation
@@ -25,7 +34,7 @@
 (setq visual-line-mode t)
 (setq word-wrap nil)
 
-;; modes for super_words and CamelCase words
+;; modes for navigating super_words and CamelCase words
 (setq global-superword-mode t)
 ; (setq global-subword-mode t)
 
@@ -40,17 +49,11 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; init use-package
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-
 (use-package better-defaults)
-(use-package clang-format)
 
 (setq inhibit-startup-message t)    ;; Hide the startup message
 (global-linum-mode t)               ;; Enable line numbers globally
-(setq electric-pair-preserve-balance nil)
+(setq electric-pair-preserve-balance t)
 
 ;; Enable auto-revert-mode
 (global-auto-revert-mode t)
@@ -59,17 +62,15 @@
 (setq-default indent-tabs-mode  nil)
 
 ;; =========================
-;; Convenience packages
+;; Appearance configs
 ;; =========================
 
 (use-package minions
   :ensure t
   :init
   (setq minions-mode-line-lighter "☰")
-  (minions-mode 1)
-  )
+  (minions-mode 1))
   
-
 ;; =========================
 ;; Global development setup
 ;; =========================
@@ -77,13 +78,12 @@
 ;; enable flycheck globally
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-
-;; (require 'company)
-;; (define-global-minor-mode my-global-company-mode company-mode
-;;   (lambda ()
-;;     (when (not (memq major-mode
-;;                      (list 'python-mode 'minibuffer-mode)))
-;;       (company-mode))))
+(use-package projectile
+  :init
+  (add-hook 'prog-mode-hook #'(lambda () (projectile-mode +1)))
+  :bind-keymap
+  ("C-c p" . 'projectile-command-map)
+  )
 
 (use-package company
   :init
@@ -101,62 +101,85 @@
   :config
   (bind-keys :map company-active-map
              ("TAB" . company-complete))
-  (setq lsp-completion-provider :capf))
+  )
 
 
 (use-package rainbow-delimiters
   :init
+  ;; turn on rainbow delims in all programming languages and LaTeX
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'LaTeX-mode-hook #'rainbow-delimiters-mode)
+
   :config
-  (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
-  (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
-  (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
-  (set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
-  (set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
-  (set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
-  (set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
-  (set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
-  (set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66") ; red
+  (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6") ; green
+  (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f") ; blue
+  (set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6") ; yellow
+  (set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc") ; cyan
+  (set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c") ; magenta
+  (set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc") ; light gray
+  (set-face-foreground 'rainbow-delimiters-depth-8-face "#999") ; medium gray
+  (set-face-foreground 'rainbow-delimiters-depth-9-face "#666") ; dark gray
   )
 
-(use-package paredit
+;; (use-package paredit
+;;   :init
+;;   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
+;;   (add-hook 'ielm-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'lisp-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
+;; )
+
+(use-package smartparens
   :init
-  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook 'enable-paredit-mode)
-  (add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
-)
+  (add-hook 'prog-mode-hook #'smartparens-mode)
+  :config
+  (smartparens-strict-mode t)
+  )
+
+
+(use-package multiple-cursors
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+  )
 
 ;; =========================
 ;; Python
 ;; =========================
 
-
 (use-package python
+  :init
+  (setq python-shell-interpreter "python3")
   :commands python-mode
   :interpreter ("python3" . python-mode)
   :custom
   (python-environment-virtualenv (quote ("python3" "-m" "venv")))
   )
-(setq python-shell-interpreter "python3")
+
 
 (use-package elpy
   :init
   (advice-add 'python-mode :before 'elpy-enable)
+  :after (python flycheck)
   :config
-  (when (require 'flycheck nil t)
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-    (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode)
   (setq elpy-rpc-python-command "python3")
   (setq elpy-rpc-backend "jedi")
-;  (setq elpy-rpc-pythonpath "/home/rego/.emacs.d/elpa/elpy-20200202.2031/")
   (setq elpy-rpc-virtualenv-path 'system)
-
-  ;; (add-hook 'elpy-mode-hook
-  ;;           (lambda () (local-set-key (kbd "C-TAB") #'company-jedi)))
   )
+
+(use-package py-autopep8
+  :after (python elpy)
+  :bind*
+  (:map elpy-mode-map ("C-c C-f" . 'py-autopep8-buffer))
+  )
+
 
 ;; jedi provides auto completion for Python programs. Depends on the
 ;; Python packages "jedi" and "epc" to be installed on the host
@@ -166,16 +189,11 @@
   (add-hook 'python-mode-hook 'jedi:setup)
   (add-hook 'python-mode-hook 'jedi:ac-setup)
   (setq jedi:complete-on-dot t)
-  (setq jedi:server-command
-        '("/home/rego/.emacs.d/.python-environments/default/bin/jediepcserver"))
-
-;  (setq jedi:setup-keys t)
+  ;; (setq jedi:server-command
+  ;;       '("/home/rego/.emacs.d/.python-environments/default/bin/jediepcserver"))
+  :after python
   )
 
-(use-package py-autopep8
-  :config
-  (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-  )
 
 ;; =========================
 ;; LaTeX
@@ -187,6 +205,7 @@
   auctex
   
   :config
+  (TeX-global-PDF-mode nil)
   (setq LaTeX-indent-environment-list
    '(("verbatim" current-indentation)
      ("verbatim*" current-indentation)
@@ -245,23 +264,25 @@
 ;; C / C++ setup
 ;; =========================
 
+(use-package clang-format)
+(use-package cc-mode
+  :init
+  (add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++17")))
+  :bind
+  (:map c++-mode-map ("C-c C-f" . 'clang-format-buffer))
+  )
 
-;; ;; clang format keybinding
-;; (require 'clang-format)
-;; (require 'cc-mode)
+
+
+;; set flycheck standard
+
 ;; (define-key c-mode-base-map (kbd "C-c C-f") 'clang-format-buffer)
 
 ;; ;; c++ modern syntax highlighting, needed??
 ;; (require 'modern-cpp-font-lock)
 ;; (modern-c++-font-lock-global-mode t)
 
-;; ;; set flycheck standard
-;; (add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++17")))
 
 ;; ;; c-style
 ;; (setq c-default-style "linux"
 ;;       c-basic-offset 4)
-
-
-
-
