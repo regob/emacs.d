0c788192dfb1c0bea0bd8d05a9c08da21a02854e
Rego A Borsodi <regobpx@gmail.com>
2024-07-31 17:15:16 +0200

    use-package config for calendar

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -2,18 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
-;; show calendar week numbers
-(copy-face font-lock-constant-face 'calendar-iso-week-face)
-(set-face-attribute 'calendar-iso-week-face nil
-                    :height 0.7)
-(setq calendar-week-start-day 1)
-(setq calendar-intermonth-text
-      '(propertize
-        (format "%2d"
-                (car
-                 (calendar-iso-from-absolute
-                  (calendar-absolute-from-gregorian (list month day year)))))
-        'font-lock-face 'calendar-iso-week-face))
+
+(use-package calendar
+  :ensure nil
+  :init
+  ;; show calendar week numbers
+  (copy-face font-lock-constant-face 'calendar-iso-week-face)
+  (set-face-attribute 'calendar-iso-week-face nil
+                      :height 0.7)
+
+  (setq calendar-week-start-day 1
+        calendar-intermonth-text
+        '(propertize
+          (format "%2d"
+                  (car
+                   (calendar-iso-from-absolute
+                    (calendar-absolute-from-gregorian (list month day year)))))
+          'font-lock-face 'calendar-iso-week-face))
+  )
 
 
 (provide 'init-utils)
