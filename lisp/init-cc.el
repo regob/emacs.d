e62f4e62addd40f43fd6578752ccf34104a6eb0e
Rego Borsodi <regobpx@gmail.com>
2024-08-07 09:54:13 +0200

    Remove some dead code

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -2,13 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (use-package clang-format
-;;   :config
-;;   (let ((pth (file-name-concat (getenv "HOME")
-;;                                ".styles/clang_format.yaml")))
-;;     (setq clang-format-style (concat "file:" pth)))
-;;   )
-
 (use-package cc-mode
   :ensure nil
   :config
@@ -19,20 +12,7 @@
   (add-hook 'c-mode-common-hook (lambda () (c-toggle-hungry-state 1)))
   (add-hook 'c-mode-common-hook 'smartparens-mode)
   ;; (add-hook 'c-mode-common-hook #'aggressive-indent-mode)
-
-  ;; :bind
-  ;; (:map c++-mode-map ("C-c C-f" . 'clang-format-buffer))
-  ;; (:map c-mode-map ("C-c C-f" . 'clang-format-buffer))
   )
-
-
-;; set flycheck standard
-
-;; (define-key c-mode-base-map (kbd "C-c C-f") 'clang-format-buffer)
-
-;; ;; c++ modern syntax highlighting, needed??
-;; (require 'modern-cpp-font-lock)
-;; (modern-c++-font-lock-global-mode t)
 
 (provide 'init-cc)
 
