13c691ca8aa8baeff449904981587e89e6d5d93a
Rego Borsodi <regobpx@gmail.com>
2024-08-24 20:10:03 +0200

    Add startup test

---
+++ test/test_startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+# Adapted from https://github.com/purcell/emacs.d/blob/master/test-startup.sh
+
+echo "Attempting startup..."
+${EMACS:=emacs} -nw --batch \
+                --eval '(progn
+                        (defvar url-show-status)
+                        (let ((debug-on-error t)
+                              (url-show-status nil)
+                              (user-emacs-directory default-directory)
+                              (user-init-file (expand-file-name "init.el"))
+                              (load-path (delq default-directory load-path)))
+                           (load-file user-init-file)
+                           ))'
+echo "Startup successful"
