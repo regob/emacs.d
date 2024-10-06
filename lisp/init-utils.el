5ac9cbf79812a2c6d7f304560f50107b0970837d
Rego Borsodi <regobpx@gmail.com>
2024-10-06 14:21:57 +0200

    init-utils: Add url browsing/search

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -32,23 +32,33 @@
 ;; Misc user functions
 ;; =========================
 
-(defun rb-jump-to-init-file ()
+(defun rb/jump-to-init-file ()
   "Open init.el in the current window."
   (interactive)
   (find-file user-init-file)
   )
 
-(defun rb-byte-recompile ()
+(defun rb/byte-recompile ()
   "Byte recompile all init files."
   (interactive)
   (byte-recompile-file user-init-file 0)
   (byte-recompile-directory (expand-file-name "lisp" user-emacs-directory) 0)
   )
 
+(defun rb/search-google (query)
+  "Run google search for QUERY in the browser."
+  (interactive "sQuery: ")
+  (let* ((url (concat "https://google.com/search?q=" (url-hexify-string query))))
+    (browse-url-firefox url)
+    )
+  )
+
 (define-prefix-command 'rb-user-keymap)
 (global-set-key (kbd "C-c 8") 'rb-user-keymap)
-(bind-key (kbd "i") 'rb-jump-to-init-file 'rb-user-keymap)
-(bind-key (kbd "c") #'rb-byte-recompile 'rb-user-keymap)
+(bind-key (kbd "i") 'rb/jump-to-init-file 'rb-user-keymap)
+(bind-key (kbd "c") #'rb/byte-recompile 'rb-user-keymap)
+(bind-key (kbd "u") #'browse-url-at-point 'rb-user-keymap)
+(bind-key (kbd "g") #'rb/search-google 'rb-user-keymap)
 
 
 
