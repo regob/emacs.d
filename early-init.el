4e817ff557df8c117d14275d66a9d9daff2eb441
Rego Borsodi <regobpx@gmail.com>
2024-10-16 19:57:10 +0200

    Ignore distro site-lisp; add lsp plist setting in early-init

--- early-init.el
+++ early-init.el
@@ -1,8 +1,20 @@
-;;; early-init.el --- Initialization before  -*- lexical-binding: t -*-
+;;; early-init.el --- Initialization before init.el  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
+;; disable package.el in favor of elpaca
 (setq package-enable-at-startup nil)
+
+;; use plists in lsp for performance
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
+(setenv "LSP_USE_PLISTS" "true")
+
+;; do not load site-lisp
+(setq load-path
+      (seq-filter
+       (lambda (pth) (not (string-match "/usr/share/emacs/site-lisp.*" pth)))
+       load-path))
+
 
 (provide 'early-init)
 
