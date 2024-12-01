fb18c16edc99f8122a52cb4d4b1ff1e14250d330
Rego Borsodi <regobpx@gmail.com>
2024-12-01 12:05:51 +0100

    Electric indent already activated in python.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -9,7 +9,6 @@
   :config
   (setq python-shell-interpreter "python3")
   (setq python-shell-interpreter-args "-i")
-  (add-hook 'python-mode-hook #'electric-indent-local-mode)
 
   ;; set compile command with current file name
   (add-hook 'python-mode-hook #'(lambda ()
