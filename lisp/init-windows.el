b62f28bb7c50e34dc619af5d8028f6c1576cc24f
Rego Borsodi <regobpx@gmail.com>
2025-01-20 22:38:18 +0100

    Replace C-x o keybind with original other-window command.

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -3,18 +3,15 @@
 ;;; Code:
 
 
-
-
-;; Make "C-x o" prompt for a target window when there are more than 2
 (use-package switch-window
   :ensure (:source "MELPA")
   :config
   (setq-default switch-window-shortcut-style 'alphabet)
   (setq-default switch-window-timeout nil)
-  (global-set-key (kbd "C-x o") 'switch-window)
+  (global-set-key (kbd "C-x o") 'other-window)
+  ;; Make "M-o" prompt for a target window when there are more than 2
   (global-set-key (kbd "M-o") 'switch-window)
   )
-
 
 (provide 'init-windows)
 
