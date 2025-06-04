825081b47970d586d1ec36581cbeb79de6de5e3c
Rego Borsodi <regobpx@gmail.com>
2025-06-04 13:15:37 +0200

    Add smarter keyboard-quit

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -26,6 +26,20 @@
 (use-package embark
   :config
   (global-set-key (kbd "C-.") 'embark-act))
+;; https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smarter/
+(defun rb-keyboard-quit ()
+  "Smater version of the built-in `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it."
+  (interactive)
+  (if (active-minibuffer-window)
+      (if (minibufferp)
+          (minibuffer-keyboard-quit)
+        (abort-recursive-edit))
+    (keyboard-quit)))
+(global-set-key [remap keyboard-quit] #'rb-keyboard-quit)
 
 
 (provide 'init-minibuffer)
