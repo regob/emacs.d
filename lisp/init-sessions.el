2266b9beccb33a17eaaa309c3e4b4a87b9bd630e
Rego Borsodi <regobpx@gmail.com>
2025-11-29 12:54:55 +0100

    Save some extra variables with savehist.
    Fix: turn on savehist-mode also.

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -1,18 +1,28 @@
 ;;; init-sessions.el ---  Save and restore editor sessions between restarts -*- lexical-binding: t -*-
 ;;; Commentary:
+;; session.el is not used anymore, since savehist provides the functionality to
+;; save some variables.
 ;;; Code:
 
 (use-package savehist
   :ensure nil
   :config
   (setq savehist-additional-variables
-        '(project--list)))
+        '(project--list
+          register-alist
+          bookmark-alist
+          search-ring
+          regexp-search-ring
+          compile-history))
+  (savehist-mode)
+  )
 
 
 (use-package recentf
   :ensure nil
   :config
   (setq recentf-max-saved-items 1000)
+  (add-to-list 'recentf-exclude "\\elpa")
   (recentf-mode)
   (add-hook 'delete-frame-functions (lambda (_) (recentf-save-list))))
 
